c4a76795f06730b400298f77a7bc7bc48735e66a
hao-phan-cs <18520216@gm.uit.edu.vn>
2019-09-27 22:27:37 +0700

    updated

--- mrecsys/reloader_service/refresher.py
+++ mrecsys/reloader_service/refresher.py
@@ -2,7 +2,6 @@ from mrecsys.utils.dataset import reload_data
 from mrecsys.factorization import trainer as f_trainer
 from mrecsys.sequence import trainer as s_trainer
 from mrecsys.utils.deploy import request_update
-from mrecsys import reloader_service
 
 
 def run():
